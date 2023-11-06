00dc7092f8ddd37a11bfc9302a3189576736ef64
Aashirbadd <60020431+Aashirbadd@users.noreply.github.com>
2023-11-05 20:13:22 -0700

    add utils class for possible utility objects & methods

---
+++ script/utils.js
@@ -0,0 +1,28 @@
+class Vector {
+    // Vector will be the data type that stores the x & y coordinates & operations on those coordinates
+
+    constructor(x = 0, y = 0){
+        this.x = x;
+        this.y = y;
+    }
+
+}
+
+class AssetLoader {
+    // Contains functions to load assets in the game
+
+    // Loop to keep loading assets (if not fully loaded)
+
+    // Load Asset function, which loads all the assets in the game
+
+    // Get Player
+
+    // Get Ball
+
+    // Get Pitch
+}
+
+
+class Canvas{
+    // Draws images that are in the game
+}
